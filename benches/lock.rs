7c5abf30b5027085bb28d820110638f39141b178
Changgyoo Park <wvwwvwwv@me.com>
2025-10-11 20:13:59 +0200

    chore(*): add wait queue bench

--- benches/lock.rs
+++ benches/lock.rs
@@ -1,3 +1,9 @@
+use std::sync::Arc;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::{Acquire, Release};
+use std::thread;
+use std::time::{Duration, Instant};
+
 use criterion::{Criterion, criterion_group, criterion_main};
 use saa::Lock;
 
@@ -23,5 +29,39 @@ fn shared_shared_unlock_unlock(c: &mut Criterion) {
     });
 }
 
-criterion_group!(lock, exclusive_unlock, shared_shared_unlock_unlock);
+fn wait_awake(c: &mut Criterion) {
+    c.bench_function("wait_awake", |b| {
+        b.iter_custom(|iters| {
+            let lock = Arc::new(Lock::default());
+            let entered = Arc::new(AtomicBool::new(false));
+            let mut acc = Duration::from_secs(0);
+            for _ in 0..iters {
+                assert!(lock.lock_sync());
+                let lock_clone = lock.clone();
+                let entered_clone = entered.clone();
+                let thread = thread::spawn(move || {
+                    let mut start = Instant::now();
+                    lock_clone.lock_sync_with(|| {
+                        entered_clone.swap(true, Release);
+                        start = Instant::now();
+                    });
+                    start.elapsed()
+                });
+                while !entered.load(Acquire) {}
+                assert!(lock.release_lock());
+                acc += thread.join().unwrap();
+                assert!(lock.release_lock());
+                assert!(entered.swap(false, Release));
+            }
+            acc
+        })
+    });
+}
+
+criterion_group!(
+    lock,
+    exclusive_unlock,
+    shared_shared_unlock_unlock,
+    wait_awake
+);
 criterion_main!(lock);
